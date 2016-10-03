7fa746d7619a40bb1d451441ea92578baa27347a
danger12001 <paul@projectcodex.co>
2016-10-03 21:36:02 +0200

    finished portfolio

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -6,6 +6,7 @@ var textPositionX;
 var horizon;
 var repositories;
 var trees = [];
+
 function preload() {
 
 
@@ -14,41 +15,72 @@ function preload() {
 function setup(){
   createCanvas(windowWidth, windowHeight);
   bg = loadImage('./images/background.jpg');
-  randomPositionX = random(50, width - 50);
-  randomPositionY = random(1, height);
-  horizon = height - height/7;
-
+  // randomPositionX = random(50, width - 50);
+  // randomPositionY = random(1, height);
+  // horizon = height - height/7;
+for(var i = 0; i < repositories.length; i++){
+  trees[i] = new tree(random(50, width - 50),height - height/7, repositories[i]);
+}
 }
 
 function draw(){
   background(bg);
-run();
-
-}
-
-
-
-function run(){
-for(var x = 0; x < repositories.length; x++){
-  renderTree(repositories[x]);
+for(var x = 0; x < trees.length; x++){
+  trees[x].run();
 }
 
 }
 
-function renderTree(repository) {
+function tree(x, y, repository){
+this.position = createVector(x, y);
+this.repository = repository;
+this.isOver = false;
+}
+tree.prototype.hover = function(){
+  var name = '', description = '', url ='';
 
-var text = createDiv(repository.name);
-text.position(randomPositionX - 90, random(0, horizon));
+var distance = dist(mouseX, mouseY, this.position.x, this.position.y);
+//
+
+
+
+if(distance < 50){
+    this.isOver = true;
+  } else {
+    this.isOver = false;
+  }
+
+
+if(this.isOver === true){
+  removeElements();
+  name = this.repository.name;
+  description = this.repository.description;
+  url = this.repository.url;
+  var info = createDiv('<p>Repository Name: ' + name +  '</p>' + '<p>Description: ' + description + '</p>' + '<p> URL: ' + url + '</p>');
+  info.position(50,50);
+  info.class('info');
+}
+
+
+
+
+
+};
+tree.prototype.run = function(){
+this.render();
+this.hover();
+};
+tree.prototype.render = function(){
 
   beginShape();
   fill(73,49,28);
-    rect(randomPositionX - 13, horizon, 25, 100);
+    rect(this.position.x - 13, this.position.y, 25, 100);
 
     fill(0,76,0);
-    triangle(randomPositionX, horizon - 75, randomPositionX - 50, horizon + 50, randomPositionX + 50, horizon + 50);
+    triangle(this.position.x, this.position.y - 75, this.position.x - 50, this.position.y + 50, this.position.x + 50, this.position.y + 50);
 
     fill(0,76,0);
-    triangle(randomPositionX, horizon - 150, randomPositionX - 50, horizon, randomPositionX + 50, horizon);
+    triangle(this.position.x, this.position.y - 150, this.position.x - 50, this.position.y, this.position.x + 50, this.position.y);
 endShape();
 
-}
+};
